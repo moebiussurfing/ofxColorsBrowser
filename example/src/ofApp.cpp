7eb5e1a2a9adedcbf88ded9cd71f3674f7ebba3c
moebius surfing <moebiussurfing@gmail.com>
2021-06-25 03:37:50 -0300

    Update ofApp.cpp

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -2,13 +2,11 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofSetFrameRate(60);
-    
-    colorBrowser.setup_colorBACK(color_BACK);// set local target color receiver
+	ofSetFrameRate(60);
 
-    colorBrowser.setup();
-
-    colorBrowser.setVisible(true);
+	colorBrowser.setup_colorBACK(color_BACK);// set local target color receiver
+	colorBrowser.setup();
+	colorBrowser.setVisible(true);
 }
 
 //--------------------------------------------------------------
@@ -18,8 +16,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackground( ofColor( color_BACK ));// auto updated when changes
-
+	ofBackground( ofColor( color_BACK ));// auto updated when changes
 	colorBrowser.draw();
 }
 
@@ -31,4 +28,3 @@ void ofApp::keyPressed(int key){
 void ofApp::windowResized(int w, int h) {
 	colorBrowser.windowResized(w, h);
 }
-
